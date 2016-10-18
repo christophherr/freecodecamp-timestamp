882fc4ce138a02c92d5db72bae874ed773bbcc28
Christoph Herr <info@christophherr.com>
2016-10-18 00:58:43 +0000

    Adds logic and view

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 var express = require('express');
+var moment = require('moment');
+var path = require('path');
+
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', process.env.PORT);
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+       res.render('index');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:timestamp', function (req,res) {
+
+    var time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+    
+    if ( ! time.isValid() ) {
+        time = moment.unix(req.params.timestamp);
+    }
+    
+    if ( ! time.isValid() ) {
+        res.json({'natural': null, 'unix': null});
+    }
+
+    
+    res.json({'natural': time.format('MMMM DD, YYYY'), 'unix': time.format('X') });
+
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node.js Server is listening on port ' + app.get('port'));
+  });
+    
+    
